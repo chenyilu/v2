2bdad20b1bb72f6a8d4945cdcb6f0e1e94e96417
chenyilu <61372333+chenyilu@users.noreply.github.com>
2020-04-08 18:26:47 +0800

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+mkdir /tmp/v2/
+
+unzip /v2.zip -d /tmp/v2/
+
+chmod 755 /tmp/v2/v2ray
+chmod 755 /tmp/v2/v2ctl
+
+cat << EOF > /tmp/v2/config.json
+{
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "alterId": 64
+                    }
+                ],
+                "disableInsecureEncryption": true
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+
+EOF
+
+#CHECK
+echo 检测文件
+cat /tmp/v2/config.json
+
+echo 开始运行
+/tmp/v2/v2ray -config /tmp/v2/config.json
+
+echo 运行结束
